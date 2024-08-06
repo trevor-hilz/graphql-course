d53654dedc356cfc27d93d552d7c3d045936c4a8
Trevor <trevorhilz@gmail.com>
2024-08-06 12:30:23 -0500

    continued work on the graphql course

--- basics/src/resolvers.js
+++ basics/src/resolvers.js
@@ -1,6 +1,56 @@
+const users = [
+  {
+    id: '1',
+    name: 'Trevor',
+    email: 'trevor@example.com',
+    age: 35,
+  },
+  {
+    id: '2',
+    name: 'Mackenzie',
+    email: 'mackenzie@example.com',
+  },
+  {
+    id: '3',
+    name: 'Bailey',
+    email: 'bailey@example.com',
+    age: 7,
+  },
+];
+
+const posts = [
+  {
+    id: '1',
+    title: 'First post',
+    body: 'This is the body of the first post.',
+    published: true,
+  },
+  {
+    id: '2',
+    title: 'Second post',
+    body: 'This is the body of the second post.',
+    published: false,
+  },
+  {
+    id: '3',
+    title: 'Third post',
+    body: 'This is the body of the third post.',
+    published: true,
+  },
+];
+
 // Resolvers
 const resolvers = {
   Query: {
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users;
+      }
+
+      return users.filter((user) => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase());
+      });
+    },
     me() {
       return {
         id: '123098',
@@ -15,6 +65,18 @@ const resolvers = {
         body: 'Hello, this is the body of the post.',
         published: true,
       };
+    },
+    posts(parent, args, ctx, info) {
+      if (!args.query) return posts;
+      return posts.filter((post) => {
+        const isTitleMatch = post.title
+          .toLowerCase()
+          .includes(args.query.toLowerCase());
+        const isBodyMatch = post.body
+          .toLowerCase()
+          .includes(args.query.toLowerCase());
+        return isTitleMatch || isBodyMatch;
+      });
     },
   },
 };
--- basics/src/typeDefs.js
+++ basics/src/typeDefs.js
@@ -1,7 +1,9 @@
 const typeDefs = `
   type Query {
+    users(query: String): [User!]!
     me: User!
     post: Post!
+    posts(query: String): [Post!]!
   }
 
   type User {
@@ -12,10 +14,10 @@ const typeDefs = `
   }
 
   type Post {
-  id: ID!
-  title: String!
-  body: String!
-  published: Boolean!
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
   }
 `;
 
